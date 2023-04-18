986782ec62bc21c8fe1249687eef3914e5533495
Mohamed El-Hasnaouy <elhasnaouymed@proton.me>
2023-04-18 09:29:22 +0000

    remove main.py

--- main.py
+++
@@ -1,8 +0,0 @@
-from flask_url_shortener.methods import create_custom_new_uid
-
-
-ts = set()
-for f in range(200000):
-    t = create_custom_new_uid(ts, 'mine', True, None)
-    print(f, t)
-    ts.add(t)
